8cf5bcf130f755744587817a5129436c27164726
Edimar Duarte Neto <153451133+EdimarNeto@users.noreply.github.com>
2023-12-10 18:20:42 -0300

    Conversor_de_Medidas.py
    Um conversor básico que trasnforma metros em outras medidas.
    A base converter that transforms meters into other measures.

---
+++ Conversor_de_Medidas.py
@@ -0,0 +1,11 @@
+m = float(input('Diga a distâcia em Metros para converter em outros valores: '))
+cm = m * 100
+mm = m * 1000
+dc = m / 100
+km = m / 1000
+print(f'Convertendo esse valor temos:'
+      f'\n{cm:.0f} centímetros'
+      f'\n{mm:.0f} milímetros')
+print('Também temos:'
+      f'\n{dc} decâmetros'
+      f'\n{km} quilômetros')
