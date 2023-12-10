5e392454a24ee17983d0f9fcf04ff3c8e8510ebc
Edimar Duarte Neto <153451133+EdimarNeto@users.noreply.github.com>
2023-12-10 18:36:04 -0300

    Conversor_de_Temperaturas.py
    Um conversor de temperaturas básico para converter celsius em fahrenheit e kelvin.
    A basic temperature converter to convert Celsius to Fahrenheit and Kelvin.

---
+++ Conversor_de_Temperaturas.py
@@ -0,0 +1,5 @@
+c = float(input('Informea temperatura em °C >>> '))
+f = (c * 9/5) + 32
+k = c + 273.15
+print(f'A temperatura de {c}°C convertida em Fahrenheit é {f}°F,'
+     f'\ne em Kelvin é {k}K.')
