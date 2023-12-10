72383d993876501b556b74f3123f069044e617a4
Edimar Duarte Neto <153451133+EdimarNeto@users.noreply.github.com>
2023-12-10 18:28:47 -0300

    Tinta_para_paredes.py
    Uma calculadora simples para medir a quantidade de tinta para pintar uma parede.
    A simple calculater to measure the amount of paint to paint a wall.

---
+++ Tinta_para_paredes.py
@@ -0,0 +1,6 @@
+lar = float(input('Qual a largura da parede: '))
+alt = float(input('Qual a altura da parede: '))
+area = larg * alt
+print(f'Sua parede têm a aréa de {area:.1f}m².'
+      '\nSabendo que cada lata de tinta cobre uma aréa de 2m²'
+      f'\nPara pintar uma parede como essa será necessário {area / 2:.1f} litros de tinta.')
