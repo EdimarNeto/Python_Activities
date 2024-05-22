8e4de2828baeac7aaa57fb3891480923598823c6
Edimar Neto <edinhoduarte.2010@gmal.com>
2024-05-22 19:52:14 -0300

    .

--- Tinta_para_paredes.py
+++ Tinta_para_paredes.py
@@ -1,6 +1,6 @@
 lar = float(input('Qual a largura da parede: '))
 alt = float(input('Qual a altura da parede: '))
-area = larg * alt
+area = lar * alt
 print(f'Sua parede têm a aréa de {area:.1f}m².'
       '\nSabendo que cada lata de tinta cobre uma aréa de 2m²'
       f'\nPara pintar uma parede como essa será necessário {area / 2:.1f} litros de tinta.')
